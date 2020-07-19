f9fa1b73afcfe365c8b25a3324d4bf6b0801f712
Arya Bhat <xxflamexxxslayerxx@gmail.com>
2020-07-19 13:06:17 +0000

    I made my final touches, re-adding a thought process that I typed out on a google doc, cleaning up problems, and fixing any things I didn't like. Overall, very happy with results.

---
+++ README.txt
@@ -0,0 +1,27 @@
+[Instructions] Create a contacts program that will allow users to:
+- Show a list of contacts
+- Find a contact (search by name)
+- Edit an existing contact
+- Add a new contact
+
+
+[Thought Process]
+- Look up ways to make dynamic lists and hotkeys in js
+- Create an thought process in mind
+- Search up more important things about functions [w3schools & stock overflow]
+- Create functions to do actions in the (yet to be made) Phonebook Menu
+- Complete functions by adding txt and depth
+- Create a function for the Phonebook menu that calls on other functions that will be used as... well, a Phonebook menu
+- Check for bugs and other problems
+- Fix said bugs and errors by doing more reasearch
+- Tidy up and add finishing touches
+- Done!
+
+
+
+
+[Checklist]
+- Show a list of contacts =done=
+- Find a contact (search by name) =done=
+- Edit an existing contact =done=
+- Add a new contact =done=
--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-'use strict';
 const _fs = require("fs");
 const readline = require('readline');
-function searchContact(contacts, name) {
-  for (let index = 0; index<contacts.length; index+=1){
-    if (contacts[index].name === name) {
-      return contacts[index];
-    }
-  }
-  return null;
+function addContact() {
+  readContacts((contacts) =>{
+    let item = {};
+    newQuestion("What is your name? : ", (name)=>{
+      item.name = name;
+      newQuestion("What is your phone number? : ", (phone)=>{
+        item.phone = phone;
+        contacts.push(item);
+        writeContacts(contacts, ()=>{
+          newMenu();
+        });
+      });
+    });
+  });
+}
+function editContact() {
+  const callback = (name) => {
+    readContacts((contacts) =>{
+      const contact = searchContact(contacts, name);
+      if (!contact) {
+        console.log("This contact cannot be found. If you would like to create a new contact, press n/N on the Phonebook Menu screen.");
+        newMenu();
+        return;
+      }
+      newQuestion("What is your new phone number? : ", (phone) => {
+        contact.phone = phone;
+        writeContacts(contacts, ()=>{
+          newMenu();
+        });
+      });
+    });
+  };
+  newQuestion("Enter the name of the contact you'd like to edit: ", callback);
 }
 function newQuestion(question, callback) {
 
@@ -33,6 +58,22 @@ function readContacts(callback) {
     callback(coontacts);
   });
 }
+function findAContact() {
+  const callback = (name) => {
+    readContacts((contacts) =>{
+      const contact = searchContact(contacts, name);
+      if (!contact) {
+        console.log("This contact cannot be found. If you would like to create a new contact, press n/N on the Phonebook Menu screen.");
+        newMenu();
+        return;
+      }
+      console.log(contact);
+      newMenu();
+    });
+  };
+
+  newQuestion("Who is the person you're searching for? : ", callback);
+}
 function writeContacts(data, callback) {
   _fs.writeFile('phone-book.json', JSON.stringify(data), function(err){
     if(err) {
@@ -51,70 +92,28 @@ function showContacts() {
     newMenu();
   });
 }
-function addContact() {
-  readContacts((contacts) =>{
-    let item = {};
-    newQuestion("What is your name? : ", (name)=>{
-      item.name = name;
-      newQuestion("What is your phone number? : ", (phone)=>{
-        item.phone = phone;
-        contacts.push(item);
-        writeContacts(contacts, ()=>{
-          newMenu();
-        });
-      });
-    });
-  });
-}
-function findAContact() {
-  const callback = (name) => {
-    readContacts((contacts) =>{
-      const contact = searchContact(contacts, name);
-      if (!contact) {
-        console.log("This contact cannot be found. If you would like to create a new contact, press n/N on the Phonebook Menu screen.");
-        newMenu();
-        return;
-      }
-      console.log(contact);
-      newMenu();
-    });
-  };
-
-  newQuestion("Who is the person you're searching for? : ", callback);
-}
-function editContact() {
-  const callback = (name) => {
-    readContacts((contacts) =>{
-      const contact = searchContact(contacts, name);
-      if (!contact) {
-        console.log("This contact cannot be found. If you would like to create a new contact, press n/N on the Phonebook Menu screen.");
-        newMenu();
-        return;
-      }
-      newQuestion("What is your new phone number? : ", (phone) => {
-        contact.phone = phone;
-        writeContacts(contacts, ()=>{
-          newMenu();
-        });
-      });
-    });
-  };
-  newQuestion("Enter the name of the contact you'd like to edit: ", callback);
+function searchContact(contacts, name) {
+  for (let index = 0; index<contacts.length; index+=1){
+    if (contacts[index].name === name) {
+      return contacts[index];
+    }
+  }
+  return null;
 }
 function newMenu() {
   console.log("  <-Phonebook Menu (Type Corresponding Hotkey To Start)->  ");
   console.log("c/C -- Show Contact List");
-  console.log("n/N -- Add New Contact");
   console.log("e/E -- Edit An Existing Contact");
+  console.log("n/N -- Add New Contact");
   console.log("s/S -- Search For A Contact");
   newQuestion("What would you like to do ? : ", function(answer){
     answer = answer.toLowerCase();
     if (answer === "c") {
       showContacts();
-    } else if (answer === "n") {
-      addContact();
     } else if (answer === "e") {
       editContact();
+    } else if (answer === "n") {
+      addContact();
     } else if (answer === "s") {
       findAContact();
     } 
